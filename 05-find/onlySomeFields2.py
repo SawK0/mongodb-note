6bd4a9dfcab825eb722071df0c1b818344247b56
SawK0 <sawzawmyohtetko@gmail.com>
2021-11-12 23:29:47 +0700

    add more note for find

---
+++ 05-find/onlySomeFields2.py
@@ -0,0 +1,26 @@
+import pymongo
+
+''' You are not allowed to specify both 0 and 1 values in the same object (except if one of the fields is the _id field). If you specify a field with the value 0, all other fields get the value 1, and vice versa:'''
+
+myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+mydb = myclient["mydatabase"]
+mycol = mydb["customers"]
+
+for x in mycol.find({}, {"address": 0}):
+  print(x)
+
+
+''' You get an error if you specify both 0 and 1 values in the same object (except if one of the fields is the _id field):'''
+
+'''
+Error Code 
+
+import pymongo
+
+myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+mydb = myclient["mydatabase"]
+mycol = mydb["customers"]
+
+for x in mycol.find({},{ "name": 1, "address": 0 }):
+  print(x)
+'''
