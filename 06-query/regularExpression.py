fc3ff917057c5afc2d8bc1cacedd97c98c6961f6
SawK0 <sawzawmyohtetko@gmail.com>
2021-11-13 22:00:15 +0700

    add regex query

---
+++ 06-query/regularExpression.py
@@ -0,0 +1,17 @@
+import pymongo
+
+myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+
+
+mydb = myclient["mydatabase"]
+mycol = mydb["customers"]
+
+#address start with S
+myquery = {"address": {"$regex":"^S"}}
+
+mydoc = mycol.find(myquery)
+
+for x in mydoc:
+    print(x)
+
+    
